c88b1c850e9db2c40d6c8ae8bd47846eebfa56fc
Reuben Michael <reubenmichael.net@protonmail.ch>
2021-07-09 10:31:14 +1000

    feat(purpleplanet.background.js): create a placeholder for the purple planet / background (future use)

---
+++ src/components/background/purpleplanet.background.js
@@ -0,0 +1,9 @@
+import { Box } from '@material-ui/core'
+
+const Background = () => {
+    <Box>
+        {/* Insert purple background card and planet design here */}
+    </Box>
+}
+
+export default Background

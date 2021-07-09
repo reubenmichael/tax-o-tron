f429485bb3493bfcb4ae20a30a913f881b764e7d
Reuben Michael <reubenmichael.net@protonmail.ch>
2021-07-09 10:28:18 +1000

    feat(twocolumns.structure.js): create columns structure

---
+++ src/components/structure/twocolumns.structure.js
@@ -0,0 +1,34 @@
+import { Fragment } from 'react'
+import { Grid } from '@material-ui/core'
+
+const twoColumnStructure = (props) => {
+    const buckets = {
+        '1': (Array.isArray(props.bucket1) ? props.bucket1 : []),
+        '2': (Array.isArray(props.bucket2) ? props.bucket2 : [])
+    }
+
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={12} md={6} key="bucket1">
+                {buckets['1'].map((component, index) => 
+                <React.Fragment>
+                    <div key={index}>
+                        {component}
+                    </div>
+                </React.Fragment>
+                )}
+            </Grid>
+            <Grid item xs={12} md={6} key="bucket2">
+            {buckets['2'].map((component, index) => 
+                <React.Fragment>
+                    <div key={index}>
+                        {component}
+                    </div>
+                </React.Fragment>
+                )}
+            </Grid>
+        </Grid>
+    )
+}
+
+export default twoColumnStructure
